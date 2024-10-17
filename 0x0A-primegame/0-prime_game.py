20af42943a90ad3a731c6672d2482808029978f8
theeacademic <nyawira22.kelly@student.cuk.ac.ke>
2024-10-17 12:03:03 +0300

    initial commit

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def sieve_of_eratosthenes(limit):
     is_prime = [True] * (limit + 1)
-    is_prime[0], is_prime[1] = False, False
+    is_prime[0], is_prime[1] = False, False  # 0 and 1 are not prime numbers
     for i in range(2, int(limit**0.5) + 1):
         if is_prime[i]:
             for j in range(i * i, limit + 1, i):
@@ -12,37 +13,33 @@ def isWinner(x, nums):
     if x <= 0 or not nums:
         return None
 
-    # Determine the maximum value in nums to compute primes up to that limit.
     max_n = max(nums)
     is_prime = sieve_of_eratosthenes(max_n)
-    
-    # Game results cache for each value of n
     game_results = {}
 
     def game_result(n):
         if n in game_results:
             return game_results[n]
-        
+
         primes = [i for i in range(2, n + 1) if is_prime[i]]
-        turn = 0  # 0 for Maria, 1 for Ben
+        turn = 0  # 0 indicates Maria's turn, 1 indicates Ben's turn
         available_numbers = set(range(1, n + 1))
 
         while primes:
             chosen_prime = primes[0]
             multiples = set(range(chosen_prime, n + 1, chosen_prime))
             available_numbers -= multiples
-
             primes = [p for p in primes if p in available_numbers]
-            turn = 1 - turn
+            turn = 1 - turn  # Alternate turns
 
-        game_results[n] = turn
-        return turn
+        game_results[n] = 1 - turn  # Last player to make a valid move
+        return game_results[n]
 
     maria_wins, ben_wins = 0, 0
 
     for n in nums:
         result = game_result(n)
-        if result == 1:
+        if result == 0:
             maria_wins += 1
         else:
             ben_wins += 1
@@ -56,4 +53,5 @@ def isWinner(x, nums):
 
 # Test the function with an example
 print("Winner:", isWinner(5, [2, 5, 1, 4, 3]))  # Expected output: "Ben"
+
 
